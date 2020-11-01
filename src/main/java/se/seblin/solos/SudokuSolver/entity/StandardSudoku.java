51efc2f6e72702aa242107cd528c1232ccad2bd2
SebastianLindfors <sebastianlindfors@gmail.com>
2020-11-01 17:41:32 +0100

    Added newConstructor and fields to StandardSudoku class

--- src/main/java/se/seblin/solos/SudokuSolver/entity/StandardSudoku.java
+++ src/main/java/se/seblin/solos/SudokuSolver/entity/StandardSudoku.java
@@ -2,6 +2,9 @@ package se.seblin.solos.SudokuSolver.entity;
 
 import se.seblin.solos.SudokuSolver.constraints.UniquenessConstraint;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StandardSudoku extends Sudoku {
 
   public StandardSudoku() {
@@ -9,14 +12,22 @@ public class StandardSudoku extends Sudoku {
     charSet = new char[] {'1','2','3','4','5','5','6','7','8','9'};
     size = charSet.length;
 
+    Map<Integer, UniquenessConstraint> rowConstraints = new HashMap();
+    Map<Integer, UniquenessConstraint> columnConstraints = new HashMap();
+    Map<Integer, UniquenessConstraint> squareConstraints = new HashMap();
+
     cellGrid = new Cell[size][size];
     for (int i = 0; i < size; i++) {
-      UniquenessConstraint rowConstraint = new UniquenessConstraint(charSet);
-
       for (int j = 0; j < size; j++) {
+        Cell newCell = new Cell(this.charSet);
+        Integer currentSquare = i/3 + j/3;
 
-        cellGrid[i][j] = new Cell(' ', this.charSet);
-        rowConstraint.addCell(cellGrid[i][j]);
+        rowConstraints.get(i).addCell(newCell);
+        columnConstraints.get(i).addCell(newCell);
+        squareConstraints.get(currentSquare).addCell(newCell);
+
+        cellGrid[i][j] = newCell;
+
       }
     }
   }
